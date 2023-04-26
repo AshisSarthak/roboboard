2d13014c949283d469207ca14eac81e639ec7742
Ashis Sarthak <ashish_sarthak@apple.com>
2023-04-26 23:42:04 +0530

    feature/add-robot-movement-other-ui-features-1
    Added move buttons for demo purpose

--- src/components/container-app/App.tsx
+++ src/components/container-app/App.tsx
@@ -3,15 +3,26 @@ import "./App.css";
 import Board from "../board/board";
 import { useMoveRobots } from "../../hooks/use-move-robots";
 import CommandsDrawer from "../commands-drawer/commands-drawer";
+import { Button } from "@mui/material";
 
 function App() {
-  const {move,turnLeft,turnRight} = useMoveRobots();
+  const { move, turnLeft, turnRight } = useMoveRobots();
   return (
     <>
       <header className="robo-header">
+        <Button variant="outlined" onClick={move}>
+          Move
+        </Button>
+        <Button variant="outlined" onClick={turnLeft}>
+          Turn Left
+        </Button>
+        <Button variant="outlined" onClick={turnRight}>
+          Turn Right
+        </Button>
         <CommandsDrawer />
       </header>
-      <Board dimension={4}/>
+
+      <Board dimension={4} />
     </>
   );
 }
